6d808cfcee49dc8358bc40749b7497292e500f45
Rodny Estrada <rrodnyestrada1@gmail.com>
2026-05-20 18:16:08 -0400

    ci: add BASE_URL env

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  base: process.env.BASE_URL || '/',
+  
   integrations: [tailwindIntegration()],
 });
