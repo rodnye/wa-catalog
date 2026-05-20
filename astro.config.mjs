037edcb5274e3d78b6e74a2a3b5f17fc7d39f610
Rodny Estrada <rrodnyestrada1@gmail.com>
2026-05-20 13:24:31 -0400

    chore: replace with Astro Tailwind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
+import tailwindIntegration from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwindIntegration()],
+});
