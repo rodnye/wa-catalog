239db96cdb4dfc91bdb99fca093b931945d97f1f
Rodny Estrada <rrodnyestrada1@gmail.com>
2026-05-20 18:16:08 -0400

    ci: add BASE_URL env

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,25 @@
 // @ts-check
 import tailwindIntegration from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
+import path from 'node:path';
+
+const PUBLIC_BASE_URL = process.env.PUBLIC_BASE_URL || '/';
+const PUBLIC_SITE = process.env.PUBLIC_SITE;
 
 // https://astro.build/config
 export default defineConfig({
+  site: PUBLIC_SITE,
   integrations: [tailwindIntegration()],
+  base: PUBLIC_BASE_URL,
+  outDir: path.join('./dist', PUBLIC_BASE_URL),
+  build: {
+    assets: 'assets',
+  },
+  vite: {
+    resolve: {
+      alias: {
+        '@/*': 'src/*',
+      },
+    },
+  },
 });
