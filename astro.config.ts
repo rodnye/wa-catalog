b85994d4f9ff9a1b4fe513b542b3e401a481ea3f
Rodny Estrada <rrodnyestrada1@gmail.com>
2026-05-24 19:04:23 -0400

    chore: add unplugin-icons

--- astro.config.ts
+++ astro.config.ts
@@ -2,9 +2,17 @@
 import tailwind from '@astrojs/tailwind';
 import preact from '@astrojs/preact';
 import { defineConfig } from 'astro/config';
+import Icons from 'unplugin-icons/vite';
+import { readFile } from 'node:fs/promises';
+import type { Options } from 'unplugin-icons';
+import { FileSystemIconLoader } from 'unplugin-icons/loaders';
 
 const PUBLIC_BASE_URL = process.env.PUBLIC_BASE_URL || '/';
 const PUBLIC_SITE = process.env.PUBLIC_SITE;
+
+const customCollections: Options['customCollections'] = {
+  assets: FileSystemIconLoader('./src/assets'),
+};
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,6 +24,13 @@ export default defineConfig({
     assets: 'assets',
   },
   vite: {
+    plugins: [
+      Icons({
+        compiler: 'jsx',
+        jsx: 'preact',
+        customCollections,
+      }),
+    ],
     resolve: {
       alias: {
         '@/*': 'src/*',
