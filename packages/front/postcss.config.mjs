5fa6f722a5ca724c3655a378735a25fea14b3711
Rodny Estrada <rrodnyestrada1@gmail.com>
2026-08-19 16:25:29 -0400

    refactor: move Tailwind and PostCSS configs to @catalog/shared

--- packages/front/postcss.config.mjs
+++ packages/front/postcss.config.mjs
@@ -1,6 +1,4 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
+import globalPostcss from "@catalog/shared/src/styles/postcss.config.mjs";
+
+export default globalPostcss;
+
